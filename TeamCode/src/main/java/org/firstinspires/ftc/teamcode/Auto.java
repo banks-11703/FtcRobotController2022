62bb5cebf53434946892e70327c45b8f35ebd4a8
Jb503 <jb-503@hotmail.com>
2022-09-13 11:16:51 -0700

    OpenCv Roadrunner and complete detection system + basic ops
    Everything I have been working on a separate repository

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+@Autonomous
+public class Auto extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Declare your drive class
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        // Set the pose estimate to where you know the bot will start in autonomous
+        // Refer to https://www.learnroadrunner.com/trajectories.html#coordinate-system for a map
+        // of the field
+        // This example sets the bot at x: 10, y: 15, and facing 90 degrees (turned counter-clockwise)
+        Pose2d startPose = new Pose2d(10, 15, Math.toRadians(90));
+
+        drive.setPoseEstimate(startPose);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        // Example spline path from SplineTest.java
+        // Make sure the start pose matches with the localizer's start pose
+        Trajectory traj = drive.trajectoryBuilder(startPose)
+                .splineTo(new Vector2d(45, 45), 0)
+                .build();
+
+        drive.followTrajectory(traj);
+
+        sleep(2000);
+
+        drive.followTrajectory(
+                drive.trajectoryBuilder(traj.end(), true)
+                        .splineTo(new Vector2d(15, 15), Math.toRadians(180))
+                        .build()
+        );
+
+        // Transfer the current pose to PoseStorage so we can use it in TeleOp
+        PoseStorage.currentPose = drive.getPoseEstimate();
+    }
+}
+
