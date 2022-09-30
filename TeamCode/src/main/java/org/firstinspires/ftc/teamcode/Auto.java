c7bb0c431af3349c70a6cfd8d1ff91a9d22e449a
Goldstormzy18 <91763425+Goldstormzy18@users.noreply.github.com>
2022-09-30 15:39:30 -0700

    Update Auto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -1,14 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
-import org.firstinspires.ftc.teamcode.CameraTesting;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
@@ -21,9 +18,10 @@ public class Auto extends CameraTesting {
     boolean button_a_was_pressed;
     boolean button_x_is_pressed = gamepad1.x;
     boolean button_x_was_pressed;
-    int team = 0;// 0 = red 1 = blue
-    int side = 0;// 0 = left 1 = right
-    int mode = 0;//0 = nothing
+    int team = 0;// 0 = red 1 = blue    which team we are on
+    int side = 0;// 0 = left 1 = right  are we left or right
+    int mode = 0;//0 = nothing          are we just parking or otherwise?
+    int autoParkPosition = 0;
 
     public int Team() {
         return team % 2;
@@ -75,117 +73,126 @@ public class Auto extends CameraTesting {
             }
         });
         while (!opModeIsActive()) {
-            if (button_b_is_pressed && !button_b_was_pressed) {
-                team++;
-                button_b_was_pressed = true;
-            } else if (!button_b_is_pressed && button_b_was_pressed) {
-                button_b_was_pressed = false;
-            }
-            if (button_a_is_pressed && !button_a_was_pressed) {
-                side++;
-                button_a_was_pressed = true;
-            } else if (!button_a_is_pressed && button_a_was_pressed) {
-                button_a_was_pressed = false;
-            }
-            if (button_x_is_pressed && !button_x_was_pressed) {
-                mode++;
-                button_x_was_pressed = true;
-            } else if (!button_x_is_pressed && button_x_was_pressed) {
-                button_x_was_pressed = false;
-            }
-            switch (Team()){
-                case (0):
-                    telemetry.addData("Team", "Red");
-                    telemetry.update();
-                    switch (Side()){
-                        case(0):
-                            telemetry.addData("Side", "Left");
-                            telemetry.update();
-                            switch(Mode()){
-                                case(0):
-                                    telemetry.addData("Mode", "Nothing");
-                                    telemetry.update();
-                                    break;
-                                case(1):
-                                    telemetry.addData("Mode", "Cycle");
-                                    telemetry.update();
-                                    break;
-                                case(2):
-                                    telemetry.addData("Mode", "Park Only");
-                                    telemetry.update();
-                                    break;
-                            }
-                            break;
-                        case(1):
-                            telemetry.addData("Side", "Right");
-                            telemetry.update();
-                            switch(Mode()){
-                                case(0):
-                                    telemetry.addData("Mode", "Nothing");
-                                    telemetry.update();
-                                    break;
-                                case(1):
-                                    telemetry.addData("Mode", "Cycle");
-                                    telemetry.update();
-                                    break;
-                                case(2):
-                                    telemetry.addData("Mode", "Park Only");
-                                    telemetry.update();
-                                    break;
-                            }
-                            break;
-                    }
-                    break;
-                case (1):
-                    telemetry.addData("Team", "Blue");
-                    telemetry.update();
-                    switch (Side()){
-                        case(0):
-                            telemetry.addData("Side", "Left");
-                            telemetry.update();
-                            switch(Mode()){
-                                case(0):
-                                    telemetry.addData("Mode", "Nothing");
-                                    telemetry.update();
-                                    break;
-                                case(1):
-                                    telemetry.addData("Mode", "Cycle");
-                                    telemetry.update();
-                                    break;
-                                case(2):
-                                    telemetry.addData("Mode", "Park Only");
-                                    telemetry.update();
-                                    break;
-                            }
-                            break;
-                        case(1):
-                            telemetry.addData("Side", "Right");
-                            telemetry.update();
-                            switch(Mode()){
-                                case(0):
-                                    telemetry.addData("Mode", "Nothing");
-                                    telemetry.update();
-                                    break;
-                                case(1):
-                                    telemetry.addData("Mode", "Cycle");
-                                    telemetry.update();
-                                    break;
-                                case(2):
-                                    telemetry.addData("Mode", "Park Only");
-                                    telemetry.update();
-                                    break;
-                            }
-                            break;
-                    }
-                    break;
-            }
-            waitForStart();
+            while(!isStarted()){
 
+
+                if (button_b_is_pressed && !button_b_was_pressed) {
+                    team++;
+                    button_b_was_pressed = true;
+                } else if (!button_b_is_pressed && button_b_was_pressed) {
+                    button_b_was_pressed = false;
+                }
+                if (button_a_is_pressed && !button_a_was_pressed) {
+                    side++;
+                    button_a_was_pressed = true;
+                } else if (!button_a_is_pressed && button_a_was_pressed) {
+                    button_a_was_pressed = false;
+                }
+                if (button_x_is_pressed && !button_x_was_pressed) {
+                    mode++;
+                    button_x_was_pressed = true;
+                } else if (!button_x_is_pressed && button_x_was_pressed) {
+                    button_x_was_pressed = false;
+                }
+                switch (Team()){
+                    case (0):
+                        telemetry.addData("Team", "Red");
+                        telemetry.update();
+                        switch (Side()){
+                            case(0):
+                                telemetry.addData("Side", "Left");
+                                telemetry.update();
+                                switch(Mode()){
+                                    case(0):
+                                        telemetry.addData("Mode", "Nothing");
+                                        telemetry.update();
+                                        break;
+                                    case(1):
+                                        telemetry.addData("Mode", "Cycle");
+                                        telemetry.update();
+                                        break;
+                                    case(2):
+                                        telemetry.addData("Mode", "Park Only");
+                                        telemetry.update();
+                                        break;
+                                }
+                                break;
+                            case(1):
+                                telemetry.addData("Side", "Right");
+                                telemetry.update();
+                                switch(Mode()){
+                                    case(0):
+                                        telemetry.addData("Mode", "Nothing");
+                                        telemetry.update();
+                                        break;
+                                    case(1):
+                                        telemetry.addData("Mode", "Cycle");
+                                        telemetry.update();
+                                        break;
+                                    case(2):
+                                        telemetry.addData("Mode", "Park Only");
+                                        telemetry.update();
+                                        break;
+                                }
+                                break;
+                        }
+                        break;
+                    case (1):
+                        telemetry.addData("Team", "Blue");
+                        telemetry.update();
+                        switch (Side()){
+                            case(0):
+                                telemetry.addData("Side", "Left");
+                                telemetry.update();
+                                switch(Mode()){
+                                    case(0):
+                                        telemetry.addData("Mode", "Nothing");
+                                        telemetry.update();
+                                        break;
+                                    case(1):
+                                        telemetry.addData("Mode", "Cycle");
+                                        telemetry.update();
+                                        break;
+                                    case(2):
+                                        telemetry.addData("Mode", "Park Only");
+                                        telemetry.update();
+                                        break;
+                                }
+                                break;
+                            case(1):
+                                telemetry.addData("Side", "Right");
+                                telemetry.update();
+                                switch(Mode()){
+                                    case(0):
+                                        telemetry.addData("Mode", "Nothing");
+                                        telemetry.update();
+                                        break;
+                                    case(1):
+                                        telemetry.addData("Mode", "Cycle");
+                                        telemetry.update();
+                                        break;
+                                    case(2):
+                                        telemetry.addData("Mode", "Park Only");
+                                        telemetry.update();
+                                        break;
+                                }
+                                break;
+                        }
+                        break;
+                }
+
+
+                autoParkPosition = pipeline.getAnalysis();
+
+            }
+
+            webcam.stopStreaming();
             if (isStopRequested()) return;
 
             telemetry.addData("Analysis", pipeline.getAnalysis());
             sleep(100);
             telemetry.update();
+            /*
             switch (Team()){
                 case (0):
                     telemetry.addData("Team", "Red");
@@ -195,15 +202,15 @@ public class Auto extends CameraTesting {
                             telemetry.addData("Side", "Left");
                             telemetry.update();
                             switch(Mode()){
-                                case(0):
+                                case 0:
                                     telemetry.addData("Mode", "Nothing");
                                     telemetry.update();
                                     break;
-                                case(1):
+                                case 1:
                                     telemetry.addData("Mode", "Cycle");
                                     telemetry.update();
                                     break;
-                                case(2):
+                                case 2:
                                     telemetry.addData("Mode", "Park Only");
                                     telemetry.update();
                                     break;
@@ -213,15 +220,15 @@ public class Auto extends CameraTesting {
                             telemetry.addData("Side", "Right");
                             telemetry.update();
                             switch(Mode()){
-                                case(0):
+                                case 0:
                                     telemetry.addData("Mode", "Nothing");
                                     telemetry.update();
                                     break;
-                                case(1):
+                                case 1:
                                     telemetry.addData("Mode", "Cycle");
                                     telemetry.update();
                                     break;
-                                case(2):
+                                case 2:
                                     telemetry.addData("Mode", "Park Only");
                                     telemetry.update();
                                     break;
@@ -271,10 +278,76 @@ public class Auto extends CameraTesting {
                             break;
                     }
                     break;
+            }*/
+            Trajectory myTrajectory;
+            switch(autoParkPosition){
+                case 1:
+                    myTrajectory = drive.trajectoryBuilder(new Pose2d())
+                            .strafeRight(24)
+                            .forward(50.6)
+                            .strafeLeft(24)
+                            .build();
+                    waitForStart();
+                    if(isStopRequested()) return;
+                    drive.followTrajectory(myTrajectory);
+                    break;
+                case 2:
+                    myTrajectory = drive.trajectoryBuilder(new Pose2d())
+                            .strafeLeft(24)
+                            .forward(26.6)
+                            .build();
+                    waitForStart();
+                    if(isStopRequested()) return;
+                    drive.followTrajectory(myTrajectory);
+                    break;
+                default:
+                    myTrajectory = drive.trajectoryBuilder(new Pose2d())
+                            .strafeRight(24)
+                            .forward(26.6)
+                            .build();
+                    waitForStart();
+                    if(isStopRequested()) return;
+                    drive.followTrajectory(myTrajectory);
+                    break;
+
             }
 
             // Transfer the current pose to PoseStorage so we can use it in TeleOp
             PoseStorage.currentPose = drive.getPoseEstimate();
         }
     }
+
+    public double calculateStartingX(){
+        if(team == 0){
+            if(side == 1){
+                return 36;
+            }else{
+                return -36;
+            }
+        }else{
+            if(side == 1){
+                return -36;
+            }else{
+                return 36;
+            }
+        }
+
+
+    }
+    public double calculateStartingY(){
+        if(team == 0){
+            return -62.6;
+        }else{
+            return 62.6;
+        }
+    }
+    public double calculateStartingRotation(){
+        if(team == 0){
+            return Math.toRadians(90);
+        }else{
+            return Math.toRadians(-90);
+        }
+
+    }
+
 }
