2fc56ce12e1a8ebbc12b7f0e375e7760e2858222
Goldstormzy18 <91763425+Goldstormzy18@users.noreply.github.com>
2023-02-10 15:49:40 -0800

    im stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveCodeNotBryce.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveCodeNotBryce.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
-public class NoAutoHomeDriveCode extends DriveCodeCommonNotBryce {
+public class DriveCodeNotBryce extends DriveCodeCommonNotBryce {
     @Override
     public void runOpMode() {
         Initialization();
